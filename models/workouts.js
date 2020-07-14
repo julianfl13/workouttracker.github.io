0c3af36d39d874cbe306f332fae449874f65894c
Julian Lopez <julian12lopez@gmail.com>
2020-07-14 17:32:37 -0400

    added api.js file, update in progress

---
+++ models/workouts.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutsSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter workout"
+  },
+  value: {
+    type: Number,
+    required: "Enter amount of weight"
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Transaction = mongoose.model("Workouts", workoutsSchema);
+
+module.exports = Transaction;
