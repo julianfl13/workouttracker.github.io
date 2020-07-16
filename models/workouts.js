9956d6ff49ecb8c7f58fe489fae5e61eae69bf04
Julian Lopez <julian12lopez@gmail.com>
2020-07-16 13:21:09 -0400

    workout.js eddited, db created

--- models/workouts.js
+++ models/workouts.js
@@ -18,6 +18,6 @@ const workoutsSchema = new Schema({
   }
 });
 
-const Transaction = mongoose.model("Workouts", workoutsSchema);
+const Workouts = mongoose.model("Workouts", workoutsSchema);
 
 module.exports = Transaction;
