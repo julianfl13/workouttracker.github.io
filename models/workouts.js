bcdbcdef1828d243fc936e89d24b8ffea4e30ac7
Julian Lopez <julian12lopez@gmail.com>
2020-07-17 13:57:42 -0400

    workouts.js edited and finished

--- models/workouts.js
+++ models/workouts.js
@@ -2,22 +2,46 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutsSchema = new Schema({
+const workoutsSchema = new Schema(
+  {  date: {
+    type: Date,
+    default: Date.now
+  },
+  exercises:[{
   name: {
     type: String,
     trim: true,
     required: "Enter workout"
   },
+  time:{
+    type: Number,
+    required:"How long was the workout"
+  },
   value: {
     type: Number,
     required: "Enter amount of weight"
   },
-  date: {
-    type: Date,
-    default: Date.now
+  weight:{
+    type: Number
+  },
+  reps:{
+    type: Number
+  },
+  sets:{
+    type: Number
   }
+}
+]
+},{toJSON:{
+  virtuals:true
+}
+}
+);
+workoutsSchema.virtual("totalTime").get(function(){
+  return this.exercises.reduce((total, exercises) => {
+    return total + exercises.time;
+  },0);
 });
-
 const Workouts = mongoose.model("Workouts", workoutsSchema);
 
 module.exports = Transaction;
